11cad37f0fe4c5c701cc8ec07bd2795901f29933
Smirnov Stanislavv <st.smirnov21@gmail.com>
2022-05-29 12:28:55 +0300

    ActorsCreators

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 	const mMenuBtn = $(".m-menu-button");
 	const mMenu = $(".m-menu");
+	const tab = $(".tab");
 
 	const openMenu = () => {
 		mMenu.toggleClass("activeMenu");
@@ -9,9 +10,38 @@ $(document).ready(function () {
 
 	mMenuBtn.on("click", openMenu);
 
+	tab.on("click", function () {
+		tab.removeClass("active");
+		$(this).toggleClass("active");
+		$(this).attr("data - target");
+		let activeTabContent = $(this).attr("data-target");
+		$(".tabs-content").removeClass("visible");
+		$(activeTabContent).toggleClass("visible");
+	});
+
 	$(function () {
 		$(".m-menu-button").click(function () {
 			$(this).toggleClass("open");
 		});
+	});
+	const swiper = new Swiper(".swiper", {
+		loop: true,
+		slidesPerView: 4,
+		spaceBetween: 25,
+		breakpoints: {
+			992: {
+				slidesPerView: 4,
+			},
+			768: {
+				slidesPerView: 2,
+			},
+			320: {
+				slidesPerView: 1,
+				slidersOffsetAfter: 50,
+				navigation: {
+					nextEl: ".button-next",
+				},
+			},
+		},
 	});
 });
